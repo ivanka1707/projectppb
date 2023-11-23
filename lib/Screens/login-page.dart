69409bcb631de8c4707f7f412063dfa31e8541f4
willy <dermawanwilly9@gmail.com>
2023-11-23 11:14:09 +0700

    tes2

--- lib/Screens/login-page.dart
+++ lib/Screens/login-page.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("asd"),
+      body: Text("qwe"),
     );
 
   }
